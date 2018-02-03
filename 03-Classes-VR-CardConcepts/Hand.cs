92212614497e979f3abd14897c7812ff62bc15a6
Zeyu <Zeyu@DESKTOP-ID9RBQP>
2018-02-02 22:52:40 -0600

    Add project files.

---
+++ 03-Classes-VR-CardConcepts/Hand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Classes_VR_CardConcepts
+{
+
+    // class for constructing a player's hand of cards
+    public class Hand
+    {
+
+        private List<Card> h;  // the cards in the hand
+
+        public Hand() { h = new List<Card>(); }
+
+        // adds Card c to the hand
+        public void add(Card c) { h.Add(c); }
+
+        // removes the Card with Suit s and Count c from the hand (if it's there)
+        public bool remove(Count c, Suit s)
+        {
+            bool ok = false;
+            foreach (Card cd in h)
+            {
+                if (cd.count == c && cd.suit == s)
+                {
+                    h.Remove(cd); ok = true;
+                    break;
+                }
+            }
+            return ok;
+        }
+
+        // returns the count of how many cards are in the hand
+        public int howManyCards() { return h.Count; }
+
+        // returns the Blackjack score of the hand
+        public int BJscore()
+        {
+            int score = 0;
+            foreach(Card cd in h)
+            {
+                score += cd.BJvalue();
+            }
+            return score;
+        }
+
+        // returns a string that lists the cards in the hahd
+        public override string ToString()
+        {
+            string ans = "";
+            foreach (Card c in h)
+            {
+                ans = ans + c.ToString() + "\n";
+            }
+            return ans;
+        }
+    }
+}
---
+++ 03-Classes-VR-UnitTests/UnitTest1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Classes_VR_CardConcepts;
+
+namespace Classes_VR_UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Card c1 = new Card(Count.Ace, Suit.Hearts);
+            Assert.AreEqual(Count.Ace, c1.count);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Card c1 = new Card(Count.Ace, Suit.Hearts);
+            Assert.AreEqual(Suit.Hearts, c1.suit);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Card c1 = new Card(Count.Ace, Suit.Hearts);
+            Assert.AreEqual(1, c1.BJvalue());
+            Console.WriteLine(c1.ToString());
+        }
+        [TestMethod]
+        public void deckTest()
+        {
+            Deck d1 = new Deck();
+            for(int i = 0; i < 24; i++)
+            {
+                Console.WriteLine(d1.deal().ToString());
+            }
+            
+            Assert.AreEqual(24, d1.count);
+            Assert.AreEqual(28, d1.left);
+        }
+        [TestMethod]
+        public void handTest()
+        {
+            Deck d1 = new Deck();
+            Hand h1 = new Hand();
+            for(int i = 0; i < 5; i++)
+            {
+                h1.add(d1.deal());
+            }
+            Console.WriteLine(h1.ToString());
+            Assert.AreEqual(5, h1.howManyCards());
+
+        }
+
+        [TestMethod]
+        public void BJTest()
+        {
+            Deck d1 = new Deck();
+            Hand h1 = new Hand();
+            Card c1 = new Card(Count.Four, Suit.Clubs);
+            Card c2 = new Card(Count.Six, Suit.Diamonds);
+            Card c3 = new Card(Count.Queen, Suit.Hearts);
+            h1.add(c1);
+            h1.add(c2);
+            h1.add(c3);
+            Assert.AreEqual(20, h1.BJscore());
+        }
+    }
+}
